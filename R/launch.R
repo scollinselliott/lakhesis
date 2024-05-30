0be837368c7e45b3627fc3cecc4d0ffb91cb5825
Stephen A. Collins-Elliott <sce@utk.edu>
2024-05-30 13:44:54 -0500

    Create launch.R

---
+++ R/launch.R
@@ -0,0 +1,18 @@
+#' Lakhesis Calculator
+#'
+#' Launch Lakhesis Calculator, a graphical interface to explore binary matrices via correspondence analysis, select potentially well-seriated sequences, and perform consensus seriation. Interface is made with \code{ggplot2}, \code{shiny}, \code{shinydashboard}, \code{bslib}  \insertCite{wickham_ggplot2:_2016,chang_shiny_2024,chang_shinydashboard_2021,sievert_bslib_2024}{lakhesis}. 
+#' 
+#' @returns Opens the Lakhesis Calculator. CSV files are imported (see \link[lakhesis]{im.csv.read} for formatting). Results are download as an \code{.Rdata} file containing a list object `results` of  the following:
+#' * `strands` The strands selected by the investigator.
+#' * `lakhesis_results` The consensus seriation achieved by applying rank PCA to the strands, including the Lakhesis coefficient of agreement between each strand and the consensus seriation (\code{\link[lakhesis]{lakhesize}}).
+#' * `im` The incidence matrix of the consensus seriation.
+#' 
+#' @references
+#'   \insertAllCited{}
+#' 
+#' @export
+#' @importFrom Rdpack reprompt
+LC <- function() {
+    shiny::runApp(appDir = system.file("app", package = "lakhesis"))
+}
+
