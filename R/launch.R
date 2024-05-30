e3743990c6f19982d277f7923d876d8abf2eba9a
Stephen A. Collins-Elliott <sce@utk.edu>
2024-05-30 13:53:55 -0500

    Update launch.R

--- R/launch.R
+++ R/launch.R
@@ -1,11 +1,16 @@
 #' Lakhesis Calculator
 #'
-#' Launch Lakhesis Calculator, a graphical interface to explore binary matrices via correspondence analysis, select potentially well-seriated sequences, and perform consensus seriation. Interface is made with \code{ggplot2}, \code{shiny}, \code{shinydashboard}, \code{bslib}  \insertCite{wickham_ggplot2:_2016,chang_shiny_2024,chang_shinydashboard_2021,sievert_bslib_2024}{lakhesis}. 
+#' Launch Lakhesis Calculator, a graphical interface to explore binary matrices via correspondence analysis, 
+#' select potentially well-seriated sequences, and perform consensus seriation. 
+#' Interface is made with \code{ggplot2}, \code{shiny}, \code{shinydashboard}, \code{bslib} 
+#' \insertCite{wickham_ggplot2:_2016,chang_shiny_2024,chang_shinydashboard_2021,sievert_bslib_2024}{lakhesis}. 
 #' 
-#' @returns Opens the Lakhesis Calculator. CSV files are imported (see \link[lakhesis]{im.csv.read} for formatting). Results are download as an \code{.Rdata} file containing a list object `results` of  the following:
+#' @returns Opens the Lakhesis Calculator. CSV files are imported (see \link[lakhesis]{im.csv.read} for formatting). 
+#' Results are download as an \code{/rds} file containing a list object `results` of  the following:
+#' * `results` The consensus seriation achieved, including coefficients of agreement and concentration 
+#' (\code{\link[lakhesis]{lakhesize}}).
 #' * `strands` The strands selected by the investigator.
-#' * `lakhesis_results` The consensus seriation achieved by applying rank PCA to the strands, including the Lakhesis coefficient of agreement between each strand and the consensus seriation (\code{\link[lakhesis]{lakhesize}}).
-#' * `im` The incidence matrix of the consensus seriation.
+#' * `im.seriated` The incidence matrix of the consensus seriation.
 #' 
 #' @references
 #'   \insertAllCited{}
