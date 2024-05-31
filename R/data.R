f300518cc41fc99319999a4818ca02d794024c9e
Stephen A. Collins-Elliott <sce@utk.edu>
2024-05-31 01:04:21 -0500

    add data from Quattro Fontanili

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Quattro Fontanili
+#'
+#' The seriation of tombs from necropoleis at Veii, primarily Quattro Fontanili, but also Valle la Fata, Vaccareccia, and Picazzano, in southern Etruria, established by \citet{close-brooks_veii_1979}.
+#'
+#' @format ## `quattrofontanili`
+#' A seriated incidence matrix of 81 rows (tombs) and 82 columns (types).
+#' \describe{
+#' Data entered from \citet[99, Fig. 3]{close-brooks_veii_1979}, an English translation of the authors' original publication in \emph{Notizie degli Scavi} (1963). Descriptions of  types may be found in that paper.
+#' }
+#' 
+#' @source
+#'   \insertAllCited{}
+#' 
+#' @examples
+#' qf <- data(quattrofontanili)
+#' im.long(qf)
+"quattrofontanili"
